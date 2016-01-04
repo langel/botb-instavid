6302ac8c08b58d09b8bbee034a83ae4723bf298b
puke7 <knoxburry@gmail.com>
2016-01-03 23:26:21 -0600

    first outputs successful

--- assets_create.php
+++ assets_create.php
@@ -1,7 +1,12 @@
 <?php
 
+# grok data
+
 $data = json_decode(file_get_contents('assets/data.json'), TRUE);
 $pal = $data['palette_data'];
+
+
+# create background
 
 function image_gradientrect($img,$x,$y,$x1,$y1,$start,$end) {
 	if($x > $x1 || $y > $y1) {
@@ -40,4 +45,85 @@ imagefilledrectangle($img, 0, 0, 1920, 1080, IMG_COLOR_TILED);
 imagedestroy($water_pattern);
 
 imagepng($img, 'assets/background.png');
+imagedestroy($img);
+
+
+# create BotB logo
+
+$img = imagecreatetruecolor(1704, 250);
+imagesavealpha($img, true);
+$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $trans);
+$text = 'battleofthebits.org';
+$font = './arial-black.ttf';
+$size = 160;
+$x = 0;
+$y = $size;
+$spacing = -20;
+function create_color($hex, $img) {
+	$r = hexdec(substr($hex,0,2));
+	$g = hexdec(substr($hex,2,2));
+	$b = hexdec(substr($hex,4,2));
+	return imagecolorallocate($img,$r,$g,$b);
+}
+for ($j = 3; $j <= 5; $j++) {
+	$color = create_color($pal['color'.$j], $img);
+	$x = 0;
+	for ($i = 0; $i < strlen($text); $i++) {
+		$bbox = imagettftext($img, $size, 0, $x, $y, $color, $font, $text[$i]);
+		$x += $spacing + ($bbox[2] - $bbox[0]);
+	}
+	$y += 20;
+}
+imagepng($img, 'assets/botblogo.png');
+imagedestroy($img);
+
+
+# noob and title
+
+$font = './merriweather_sans.ttf';
+$img = imagecreatetruecolor(1096,246);
+imagesavealpha($img, true);
+$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $trans);
+$color = create_color($pal['color5'], $img);
+$size = 72;
+$y = floor($size * 1.1);
+$text = $data['botbr_data']['name'].' - '.$data['title'];
+$text = wordwrap($text, 25, "\n");
+imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
+imagepng($img, 'assets/title.png');
+imagedestroy($img);
+
+
+# format title
+
+$font = './merriweather_sans.ttf';
+$img = imagecreatetruecolor(996,96);
+imagesavealpha($img, true);
+$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $trans);
+$color = create_color($pal['color5'], $img);
+$size = 56;
+$y = floor($size * 1.1);
+$text = $data['format_title'];
+imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
+imagepng($img, 'assets/format-title.png');
+imagedestroy($img);
+
+
+# battle and time
+
+$font = './merriweather_sans.ttf';
+$img = imagecreatetruecolor(1096,246);
+imagesavealpha($img, true);
+$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $trans);
+$color = create_color($pal['color5'], $img);
+$size = 32;
+$y = floor($size * 1.1);
+$text = 'submitted to '.$data['battle_data']['title'].' on '.$data['datetime'];
+$text = wordwrap($text, 40, "\n");
+imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
+imagepng($img, 'assets/battle-time.png');
 imagedestroy($img);
--- instavid.sh
+++ instavid.sh
@@ -9,25 +9,31 @@ php -f assets_create.php
 
 # cp assets/background.png ~/botb/styles/img/bg.png
 
-echo "rendering video"
-echo
+echo -e "rendering video\n"
 
-ffmpeg -i assets/background.png -i assets/avatar500-%03.png -i assets/battle-art300-%03d.png -i assets/format-icon96-%03d.png -i assets/mp3file -filter_complex "
+ffmpeg -i assets/background.png -i assets/avatar500-000.png -i assets/battle-art300-%03d.png -i assets/format-icon96-%03d.png -i assets/botblogo.png -i assets/title.png -i assets/format-title.png -i assets/battle-time.png -i assets/mp3 -filter_complex "
 nullsrc=size=1920x1080 [base];
 [0:v] setpts=PTS-STARTPTS [bg];
 [1:v] setpts=PTS-STARTPTS [avatar];
 [2:v] setpts=PTS-STARTPTS [battle];
 [3:v] setpts=PTS-STARTPTS [format];
+[4:v] setpts=PTS-STARTPTS [botblogo];
+[5:v] setpts=PTS-STARTPTS [title];
+[6:v] setpts=PTS-STARTPTS [format_title];
+[7:v] setpts=PTS-STARTPTS [battle_time];
 [base][bg] overlay=shortest=1 [tmp1];
 [tmp1][avatar] overlay=shortest=1:x=1312:y=108 [tmp2];
 [tmp2][battle] overlay=shortest=1:x=108:y=408 [tmp3];
-[tmp3][format] overlay=shortest=1:x=462:y=462
+[tmp3][format] overlay=shortest=1:x=462:y=462 [tmp4];
+[tmp4][botblogo] overlay=shortest=1:x=108:y=780 [tmp5];
+[tmp5][title] overlay=shortest=1:x=108:y=108 [tmp6];
+[tmp6][format_title] overlay=shortest=1:x=570:y=480 [tmp7];
+[tmp7][battle_time] overlay=shortest=1:x=462:y=608
 " -c:v libx264 -b:v 3500k -c:a aac -strict experimental -b:a 192k -pix_fmt yuv420p -t 15 -r 30000/1001 out.mp4
 
+echo -e "moving output to derpdeck\n"
 cp out.mp4 ~/derpdeck/ 
 
-# ffmpeg -loop 1 -f image2 -i b-knox%03d.png -i output.mp3 -shortest -c:v libx264 -b:v 3500k -c:a aac -strict experimental -b:a 192k -pix_fmt yuv420p -t 15 -r 30000/1001 out.mp4
-
-# cp out.mp4 ~/botb/styles/img/ 
-
-# rm -rf assets
+echo -e "cleaning up mess\n"
+rm -rf assets
+rm out.mp4
