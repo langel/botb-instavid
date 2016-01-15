02b9a4c706ad6100d12cae65a9d737d5d22a807e
puke7 <knoxburry@gmail.com>
2016-01-14 19:17:15 -0600

    almost releasable layout!!  :D/

--- assets_create.php
+++ assets_create.php
@@ -34,12 +34,13 @@ function image_gradientrect($img,$x,$y,$x1,$y1,$start,$end) {
 	return true;
 }
 $imgWidth = 1920;
-$imgHeight = 1920;
+$imgHeight = 1080 + floor($argv[1] * 20);
 $img = imagecreatetruecolor($imgWidth,$imgHeight);
-image_gradientrect($img,0,0,$imgWidth,$imgHeight, $pal['color1'], $pal['color2']);
+image_gradientrect($img,0,0,$imgWidth,floor($imgHeight*0.5), $pal['color5'], $pal['color4']);
+image_gradientrect($img,0,ceil($imgHeight*0.5),$imgWidth,$imgHeight, $pal['color4'], $pal['color3']);
 $water_pattern = imagecreatefrompng('assets/botb_bg.png');
 imagesettile($img, $water_pattern);
-imagefilledrectangle($img, 0, 0, 1920, 1920, IMG_COLOR_TILED);
+imagefilledrectangle($img, 0, 0, $imgWidth, $imgHeight, IMG_COLOR_TILED);
 imagedestroy($water_pattern);
 imagepng($img, 'assets/background.png');
 imagedestroy($img);
@@ -63,9 +64,9 @@ for ($k = 0; $k < 5; $k++) {
 	$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
 	imagefill($img, 0, 0, $trans);
 	$y = $size;
-	for ($j = 0; $j < 4; $j++) {
+	for ($j = 0; $j < 5; $j++) {
 		$color = ($k + $j) % 5 + 1;
-		if ($j == 3) $color = 5;
+		if ($j == 4) $color = 2;
 		print $pal['color'.$color].' ';
 		$color = create_color($pal['color'.$color], $img);
 		$x = 0;
@@ -73,10 +74,13 @@ for ($k = 0; $k < 5; $k++) {
 			$bbox = imagettftext($img, $size, 0, $x, $y, $color, $font, $text[$i]);
 			$x += $spacing + ($bbox[2] - $bbox[0]);
 		}
+		/*
 		if ($j == 0) $y+= 18;
 		if ($j == 1) $y+= 11;
 		if ($j == 2) $y+= 7;
 		if ($j == 3) $y+= 4;
+		*/
+		$y+=8;
 	}
 	imagepng($img, 'assets/botblogo-'.$k.'.png');
 	imagedestroy($img);
@@ -86,19 +90,28 @@ for ($k = 0; $k < 5; $k++) {
 
 $font = './merriweather_sans.ttf';
 $font = './Racing_Sans_One/RacingSansOne-Regular.ttf';
+$size = 72;
+$scale_max = 2;
+$title_text = wordwrap($data['title'], 22, "\n");
+$title_dim = imagettfbbox($size, 0, $font, $title_text);
+$noob_text = '  '.$data['botbr_data']['name'];
+$noob_dim = imagettfbbox($size, 0, $font, $noob_text);
+$title_width = $title_dim[2];
+$title_height = $title_dim[3];
+$max_width = 1920 - 716 - 108;
+$max_height = 1080 - 108 - 608;  // 608 is questionable
+$scale_x = $max_width / $title_width;
+$scale_y = $max_height / $title_height;
+$size *= min($scale_x, $scale_y, $scale_max);
 $img = imagecreatetruecolor(1096,600);
 imagesavealpha($img, true);
 $trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
 imagefill($img, 0, 0, $trans);
-$color = create_color($pal['color5'], $img);
-$size = 72;
+$color = create_color($pal['color1'], $img);
 $y = floor($size * 1.1);
-$text = $data['title'];
-$text = wordwrap($text, 22, "\n");
-$bbox = imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
-$color = create_color($pal['color4'], $img);
-$text = '  '.$data['botbr_data']['name'];
-imagettftext($img, $size, 0, 0, $bbox[3]+$y, $color, $font, $text); 
+$bbox = imagettftext($img, $size, 0, 0, $y, $color, $font, $title_text); 
+$color = create_color($pal['color2'], $img);
+imagettftext($img, $size, 0, 0, $bbox[3]+$y, $color, $font, $noob_text); 
 imagepng($img, 'assets/title.png');
 imagedestroy($img);
 
@@ -106,31 +119,36 @@ imagedestroy($img);
 # format title
 
 $font = './Passion_One/PassionOne-Regular.ttf';
-$img = imagecreatetruecolor(996,96);
+$size = 32;
+$text = $data['format_title'];
+$text_dim = imagettfbbox($size, 0, $font, $text);
+$img = imagecreatetruecolor($text_dim[2]+88,96);
 imagesavealpha($img, true);
 $trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
 imagefill($img, 0, 0, $trans);
-$color = create_color($pal['color5'], $img);
-$size = 32;
+$color = create_color($pal['color1'], $img);
 $y = floor($size * 1.1);
-$text = $data['format_title'];
-imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
-imagepng($img, 'assets/format-title.png');
+imagettftext($img, $size, 0, 88, $y+12, $color, $font, $text); 
+$icon = imagecreatefrompng('assets/format_icon.png');
+imagecopymerge($img, $icon, 0, 0, 0, 0, imagesx($icon), imagesy($icon), 100);
+imagedestroy($icon);
+imagepng($img, 'assets/format.png');
 imagedestroy($img);
 
 
 # battle and time
 
-$img = imagecreatetruecolor(1096,246);
-imagesavealpha($img, true);
-$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
-imagefill($img, 0, 0, $trans);
-$color = create_color($pal['color5'], $img);
 $size = 32;
-$y = floor($size * 1.1);
 $text = 'submitted to '.$data['battle_data']['title'];
 $text = wordwrap($text, 50, "\n");
 $text .= "\n".'                           on '.substr($data['datetime'], 0, 10);
+$text_dim = imagettfbbox($size, 0, $font, $text);
+$img = imagecreatetruecolor($text_dim[2], 246);
+imagesavealpha($img, true);
+$trans = imagecolorallocatealpha($img, 0, 0, 0, 127);
+imagefill($img, 0, 0, $trans);
+$color = create_color($pal['color1'], $img);
+$y = floor($size * 1.1);
 imagettftext($img, $size, 0, 0, $y, $color, $font, $text); 
 imagepng($img, 'assets/battle-time.png');
 imagedestroy($img);
