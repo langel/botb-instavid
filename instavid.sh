478c5cffe6ab98beac099b6e08ddba1a300d91bf
langel <knoxburry@gmail.com>
2020-08-25 21:12:06 -0400

    reordered ffmpeg assets; avatar last

--- instavid.sh
+++ instavid.sh
@@ -24,22 +24,22 @@ echo -e "media length :: $length seconds\n"
 php -f assets_create.php $length
 
 echo -e "rendering video\n"
-ffmpeg -i assets/background.png $loop -i assets/avatar500 -i assets/battle-art300-%03d.png -i assets/format.png -loop 1 -r 15 -i assets/botblogo-%01d.png -i assets/title.png -i assets/battle-time.png -i assets/mp3 -filter_complex "
+ffmpeg -i assets/background.png -i assets/battle-art300-%03d.png -i assets/format.png -loop 1 -r 15 -i assets/botblogo-%01d.png -i assets/title.png -i assets/battle-time.png $loop -i assets/avatar500 -i assets/mp3 -filter_complex "
 nullsrc=size=1920x1080 [base];
 [0:v] setpts=PTS-STARTPTS [bg];
-[1:v] setpts=PTS-STARTPTS [avatar];
-[2:v] setpts=PTS-STARTPTS [battle];
-[3:v] setpts=PTS-STARTPTS [format];
-[4:v] setpts=PTS-STARTPTS [botblogo];
-[5:v] setpts=PTS-STARTPTS [title];
-[6:v] setpts=PTS-STARTPTS [battle_time];
+[1:v] setpts=PTS-STARTPTS [battle];
+[2:v] setpts=PTS-STARTPTS [format];
+[3:v] setpts=PTS-STARTPTS [botblogo];
+[4:v] setpts=PTS-STARTPTS [title];
+[5:v] setpts=PTS-STARTPTS [battle_time];
+[6:v] setpts=PTS-STARTPTS [avatar];
 [base][bg] overlay=y=-'((t+0.001)/$length)*(h-H)' [tmp1];
-[tmp1][avatar] overlay=1:x=108:y=108 [tmp2];
-[tmp2][battle] overlay=1:x=1362:y=522 [tmp3];
-[tmp3][format] overlay=1:x='960-(overlay_w-50)*0.5':y=562 [tmp4];
-[tmp4][botblogo] overlay=1:x=100:y=830 [tmp5];
-[tmp5][title] overlay=1:x=691:y=108 [tmp7];
-[tmp7][battle_time] overlay=1:x='960-(overlay_w+100)*0.5':y=666
+[tmp1][battle] overlay=1:x=1362:y=522 [tmp2];
+[tmp2][format] overlay=1:x='960-(overlay_w-50)*0.5':y=562 [tmp3];
+[tmp3][botblogo] overlay=1:x=100:y=830 [tmp4];
+[tmp4][title] overlay=1:x=691:y=108 [tmp5];
+[tmp5][battle_time] overlay=1:x='960-(overlay_w+100)*0.5':y=666 [ava00];
+[ava00][avatar] overlay=1:x=108:y=108
 " -c:v libx264 -b:v 3500k -c:a aac -strict experimental -b:a 192k -pix_fmt yuv420p -r 30000/1001 -t "${length}" "${entry_id}.mp4"
 
 echo -e "cleaning up mess\n"
